96eb8ecf4d3a78f834ca0f956fbcf7e8a236b236
Saúl Nogueras <susensio@gmail.com>
2026-09-15 08:09:16 +0200

    refactor: drop dead re-exports from the sources package init

--- maniac/sources/__init__.py
+++ maniac/sources/__init__.py
@@ -1,17 +1,1 @@
 """CLI help and upstream documentation source extraction package."""
-
-from .crawler import (
-    extract_subcommands,
-    find_subcommands,
-    format_help_block,
-    get_help,
-)
-from .resolution import discover_repo
-
-__all__ = [
-    "discover_repo",
-    "extract_subcommands",
-    "find_subcommands",
-    "format_help_block",
-    "get_help",
-]
